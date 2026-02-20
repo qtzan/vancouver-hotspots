445b6abf2a174c49167b8ed8b914318c2e1a0850
zan <zt0204@students.cs.ubc.ca>
2026-02-20 12:56:09 -0800

    created sql table

---
+++ sql/1_create_tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE crime_incidents_raw (
+	type TEXT,
+	year INT,
+	month INT,
+	day INT,
+	hour INT,
+	minute INT,
+	hundred_block TEXT,
+	neighbourhood TEXT,
+	x DOUBLE PRECISION,
+	y DOUBLE PRECISION
+);
